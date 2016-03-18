9f3e439a86db1e4a8fdabfc31ea91a3abb5c2e2f
Dan Hough <daniel.hough@gmail.com>
2016-03-18 13:51:08 +0000

    Added support for more repositories Delegates responsibility of certain config keys to repo-level, and also allows config headers to include username and repo name to be more specific and support multiple repos in a single running instance of DTT

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import flask
 from flask import Flask, request, jsonify
 import subprocess
@@ -25,6 +27,16 @@ def root():
     return "<html><body><h1>Do The Thing</h1></body></html>"
 
 
+@app.route("/🙂")
+def smile():
+    return "<!DOCTYPE html><html><body><h1>Smile</h1></body></html>"
+
+
+@app.route("/🙏")
+def please():
+    return "<!DOCTYPE html><html><body><h1>Please</h1></body></html>"
+
+
 @app.route("/handle-push", methods=['POST'])
 def handle_push():
 
@@ -34,7 +46,9 @@ def handle_push():
         secret_key = config['DEFAULT']['SecretKey']
         text_body = request.get_data()
         github_signature = request.headers['x-hub-signature']
+        print(secret_key)
         print(github_signature)
+        print(text_body)
         if not validate_signature(secret_key, text_body, github_signature):
             return jsonify(success=False, message='Invalid GitHub signature'), 403
 
@@ -42,17 +56,36 @@ def handle_push():
         # Gather more config data
         working_directory = config['DEFAULT']['WorkingDirectory']
         path_to_use = config['DEFAULT']['GlobalPath']
-        deploy_key_location = config['DEFAULT']['DeployKeyLocation']
+        if 'DeployKeyLocation' in config['DEFAULT']:
+            deploy_key_location = config['DEFAULT']['DeployKeyLocation']
+        else:
+            deploy_key_location = None
         resp = flask.Response(request.data)
         resp.headers['content-type'] = 'application/json'
-        json_body = request.get_json()
+        reqbody = request.get_json()
         branch = None
+        repository = None
+        config_section = None
 
-        if 'refs/heads' in json_body['ref']:
-            branch = json_body['ref'].split('/')[2]
+        if 'refs/heads' not in reqbody['ref']:
+            return jsonify(success=False, message='Missing reference'), 400
 
-        if branch != None:
-            fetch_cmd = "/usr/bin/ssh-agent /bin/sh -c '/usr/bin/ssh-add %s; /usr/bin/git fetch origin %s'" % (deploy_key_location, branch)
+        if ('repository' not in reqbody) or ('full_name' not in reqbody['repository']):
+            return jsonify(success=False, message='Missing repo info'), 400
+
+        branch = reqbody['ref'].split('/')[2]
+        repository = reqbody['repository']['full_name']
+        config_section = repository + '/' + branch
+
+        # Load repo-specific configuration
+        if config[config_section]['WorkingDirectory']:
+            working_directory = config[config_section]['WorkingDirectory']
+
+        if branch is not None:
+            if deploy_key_location is not None:
+                fetch_cmd = "/usr/bin/ssh-agent /bin/sh -c '/usr/bin/ssh-add %s; /usr/bin/git fetch origin %s'" % (deploy_key_location, branch)
+            else:
+                fetch_cmd = "/usr/bin/git fetch origin %s'" % branch
             print("Running %s" % fetch_cmd)
             git_process = subprocess.Popen(
                 fetch_cmd,
@@ -60,10 +93,10 @@ def handle_push():
                 shell=True, env=os.environ.copy()
             )
             git_process.communicate()
-            git_process = subprocess.Popen(' '.join(['/usr/bin/git', 'checkout', json_body['after']]),
+            git_process = subprocess.Popen(' '.join(['/usr/bin/git', 'checkout', reqbody['after']]),
                                            cwd=working_directory, shell=True, env=os.environ.copy())
 
-        commands = json.loads(config.get(branch, "commands"))
+        commands = json.loads(config.get(config_section, "commands"))
 
         for command in commands:
             print("Running %s" % command)
